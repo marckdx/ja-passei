42fabc5091517ae4921ccef563c1c110a96d8944
Marco Aurélio Lima <marcolima.tilog@gmail.com>
2014-11-26 17:41:01 -0200

    Inicio do projeto

---
+++ app/src/androidTest/java/fatecpg/com/br/japassei/ApplicationTest.java
@@ -0,0 +1,13 @@
+package fatecpg.com.br.japassei;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
